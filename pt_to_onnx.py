eaab7a299ea412976028753af44cefd1f8206a6d
Estiven Leonel Oña Chicaiza <elona@espe.edu.ec>
2025-01-27 15:41:36 -0500

    Delete pt_to_onnx.py

--- pt_to_onnx.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Carga el modelo entrenado
-model = YOLO('best.pt')
-
-# Convierte el modelo a ONNX
-model.export(format='onnx')  # Esto exporta el modelo a un archivo .onnx
